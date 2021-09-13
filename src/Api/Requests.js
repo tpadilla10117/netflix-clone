4f73388a7e3e10d73396b9ac90eadf919942ad56
Trinidad Padilla <tpadilla101@live.com>
2021-09-13 10:17:08 -0700

    finished templating the get reqs

--- src/Api/Requests.js
+++ src/Api/Requests.js
@@ -2,7 +2,15 @@ const API_KEY = "3b1e7eb612fd67ed6193349b0fa88ebe";
 
 const requests = {
 
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovie: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`, 
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`, 
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+
 
 };
 
