8296cb044959f3b6a586e925d50bdebf94f54b52
Trinidad Padilla <tpadilla101@live.com>
2021-09-13 09:24:45 -0700

    styling banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,20 @@ function Banner() {
 
         <div className="banner_contents">
             <h1 className="banner_title">Movie Name</h1>
+
+            <div className="banner_buttons">
+                <button className="banner_button">Play</button>
+                <button className="banner_button">My List</button>
+            </div>
+
+            <h1 className="banner_description">This is a Test Description</h1>
+
+
         </div>
+
+        <div className="banner--fadeButton" />
+
+       
         
 
         </header>
