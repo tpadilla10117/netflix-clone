b64f0eaebd2b9bf97132f683681c91fc04220d61
Trinidad Padilla <tpadilla101@live.com>
2021-09-14 12:27:30 -0700

    styled the sign in component

--- src/components/screens/SignupScreen.js
+++ src/components/screens/SignupScreen.js
@@ -1,9 +1,19 @@
 import React from "react";
+import './SignupScreen.css';
 
 function SignupScreen () {
-    return(
-        <div>
+    return (
+        <div className="signupScreen">
+            <form>
+                <h1>Sign In</h1>
+                <input placeholder="Email" type="email" />
+                <input placeholder="Password" />
+                <button type="submit">Sign In</button>
 
+                <h4> <span className="signupScreen_gray">New to Netflix? </span>
+                <span className="signupScreen_link">Sign Up now.</span>
+                </h4>
+            </form>
         </div>
     );
 };
