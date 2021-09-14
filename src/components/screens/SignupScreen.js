ad936a3a8eaf78ca40ade47b100633ee055c18ec
Trinidad Padilla <tpadilla101@live.com>
2021-09-14 15:03:30 -0700

    began setting up authentication in the app

--- src/components/screens/SignupScreen.js
+++ src/components/screens/SignupScreen.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './SignupScreen.css';
 
 function SignupScreen () {
+
+
+    const register = (event) => {
+        event.preventDefault();
+    };
+
+    const signIn = (event) => {
+        event.preventDefault();
+    };
+
+
+
     return (
         <div className="signupScreen">
             <form>
                 <h1>Sign In</h1>
                 <input placeholder="Email" type="email" />
                 <input placeholder="Password" />
-                <button type="submit">Sign In</button>
+                <button type="submit" onClick={signIn}>Sign In</button>
 
                 <h4> <span className="signupScreen_gray">New to Netflix? </span>
-                <span className="signupScreen_link">Sign Up now.</span>
+                <span className="signupScreen_link" onClick={register}>Sign Up now.</span>
                 </h4>
             </form>
         </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+    import firebase from "firebase";
+
+
 /* Firebase configuration object containing keys and identifiers for the app: */
 
     const firebaseConfig = {
@@ -8,3 +11,15 @@
         messagingSenderId: "365349534996",
         appId: "1:365349534996:web:df4e9c8d3f53b86b359601"
     };
+
+/* Initialize the firebase App: */
+    const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+/*  Initialize the realtime firestore database: */
+    const db = firebaseApp.firestore();
+
+/*  Initialize authentication for the app:*/
+    const auth = firebase.auth();
+
+    export { auth };
+    export default db;
