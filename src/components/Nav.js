5123e2d6f084ba39f469534c0bc6b3121b61c20b
Trinidad Padilla <tpadilla101@live.com>
2021-09-15 16:19:46 -0700

    styling the ProfileScreen component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -32,6 +32,7 @@ function Nav() {
             <div className="nav_contents">
 
                 <img 
+                    onClick={ () => history.push('/')}
                     className="nav_logo"
                     src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
                     alt=""
--- src/components/screens/ProfileScreen.js
+++ src/components/screens/ProfileScreen.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import {Nav} from "../../utils";
 import "./ProfileScreen.css"
+import { useSelector } from "react-redux";
+import { selectUser } from "../../features/userSlice";
 
 function ProfileScreen () {
-    return (
-        <div>
 
+    const user = useSelector(selectUser); //we grab the userSlice to populate the logged in users info
+
+
+    return (
+        <div className="profileScreen">
+            <Nav />
+
+            <div className="profileScreen_body">
+                <h1>Edit Profile</h1>
+                <div className="profileScreen_info">
+                    <img
+                        src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+                        alt=""
+                    />
+                </div>
+                <div className="profileScreen_details">
+                    <h2>{user.email}</h2>
+                </div>
+            </div>
         </div>
     );
 };
